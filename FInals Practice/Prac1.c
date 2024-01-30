c8e11ff535cf4cb6460e10e346a45d12f549c5ce
iamsohaib7 <112327237+iamsohaib7@users.noreply.github.com>
2024-01-30 10:23:34 +0000

    Code Formated

--- FInals Practice/Prac1.c
+++ FInals Practice/Prac1.c
@@ -1,66 +1,57 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 
-typedef struct
-{
-    long long start;
-    long long end;
-    long long result;
+typedef struct {
+  long long start;
+  long long end;
+  long long result;
 } ThreadArgs;
-void *factorialPart(void *args)
-{
-    ThreadArgs *arg = (ThreadArgs *)args;
-    arg->result = 1;
-    for (long long i = arg->start; i <= arg->end; i++)
-    {
-        arg->result *= i;
-    }
-    return NULL;
+void *factorialPart(void *args) {
+  ThreadArgs *arg = (ThreadArgs *)args;
+  arg->result = 1;
+  for (long long i = arg->start; i <= arg->end; i++) {
+    arg->result *= i;
+  }
+  return NULL;
 }
-int main(int argc, char *argv[])
-{
-    if (argc != 3)
-    {
-        fprintf(stderr, "Usage: %s <number> <thread_count>\n", argv[0]);
-        return 1;
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s <number> <thread_count>\n", argv[0]);
+    return 1;
+  }
+
+  long long number = atoll(argv[1]);
+  int threadCount = atoi(argv[2]);
+
+  pthread_t threads[threadCount];
+  ThreadArgs args[threadCount];
+  long long blockSize = number / threadCount;
+  long long remaining = number % threadCount;
+  printf("Remaining: %lld\n", remaining);
+  printf("Block Size: %lld\n", blockSize);
+  long long start = 1;
+  for (int i = 0; i < threadCount; i++) {
+    args[i].start = start;
+    args[i].end = start + blockSize - 1;
+    if (remaining > 0) {
+      args[i].end++;
+      remaining--;
     }
+    start = args[i].end + 1;
 
-    long long number = atoll(argv[1]);
-    int threadCount = atoi(argv[2]);
-
-    pthread_t threads[threadCount];
-    ThreadArgs args[threadCount];
-    long long blockSize = number / threadCount;
-    long long remaining = number % threadCount;
-    printf("Remaining: %lld\n", remaining);
-    printf("Block Size: %lld\n", blockSize);
-    long long start = 1;
-    for (int i = 0; i < threadCount; i++)
-    {
-        args[i].start = start;
-        args[i].end = start + blockSize - 1;
-        if (remaining > 0)
-        {
-            args[i].end++;
-            remaining--;
-        }
-        start = args[i].end + 1;
-
-        if (pthread_create(&threads[i], NULL, factorialPart, &args[i]) != 0)
-        {
-            perror("Failed to create thread");
-            return 1;
-        }
+    if (pthread_create(&threads[i], NULL, factorialPart, &args[i]) != 0) {
+      perror("Failed to create thread");
+      return 1;
     }
+  }
 
-    long long totalResult = 1;
-    for (int i = 0; i < threadCount; i++)
-    {
-        pthread_join(threads[i], NULL);
-        totalResult *= args[i].result;
-    }
+  long long totalResult = 1;
+  for (int i = 0; i < threadCount; i++) {
+    pthread_join(threads[i], NULL);
+    totalResult *= args[i].result;
+  }
 
-    printf("Factorial of %lld is %lld\n", number, totalResult);
-    return 0;
+  printf("Factorial of %lld is %lld\n", number, totalResult);
+  return 0;
 }
